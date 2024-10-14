4ebce53bf2422f07ef8da94f78500850d3aef567
webmastervino <webmastervinothkumar@gmail.com>
2024-10-14 18:51:19 -0400

    JPA Basic implementation

---
+++ src/main/java/com/webmastervino/springboot/learn_spring_boot/jpa/model/Course.java
@@ -0,0 +1,69 @@
+package com.webmastervino.springboot.learn_spring_boot.jpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    private long id;
+    private String name;
+    private String author;
+    private String description;
+
+    public Course() {
+        
+    }
+    public Course(long id, String name, String author, String description) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.description = description;
+    }
+
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
