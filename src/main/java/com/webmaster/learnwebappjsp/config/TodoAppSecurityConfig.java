8a118a72d2938c3703723c417286ecc7a3a21a27
webmastervino <webmastervinothkumar@gmail.com>
2024-11-10 23:17:52 -0500

    Enable SecurityFilterChain

--- src/main/java/com/webmaster/learnwebappjsp/config/TodoAppSecurityConfig.java
+++ src/main/java/com/webmaster/learnwebappjsp/config/TodoAppSecurityConfig.java
@@ -1,14 +1,18 @@
 package com.webmaster.learnwebappjsp.config;
 
 import java.util.function.Function;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class TodoAppSecurityConfig {
@@ -21,7 +25,7 @@ public class TodoAppSecurityConfig {
     }
 
     private UserDetails createUser(String username, String password) {
-        Function <String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
         UserDetails user = User.builder().passwordEncoder(passwordEncoder).username(username).password(password)
                 .roles("USER", "ADMIN").build();
         return user;
@@ -30,5 +34,15 @@ public class TodoAppSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
     }
 }
